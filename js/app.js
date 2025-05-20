911542047a14505f34a3a0fe2475ca5796bd71a8
Ansor <ansorgulomov4@gmail.com>
2025-05-20 17:40:29 +0500

    sham

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ let elMenyu = document.querySelector(".menyu");
 let sideBar = document.querySelector(".sidebar");
 
 elMenyu.addEventListener("click", function(){
-    
+    elMenyu.classList.toggle("svag")
 })
 
