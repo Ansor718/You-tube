1dc57cbdbc83e09cece8f6e7a4455b487342cd7b
Ansor <ansorgulomov4@gmail.com>
2025-05-21 10:17:03 +0500

    kop narsani qildim

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
 let elMenyu = document.querySelector(".menyu");
 let sideBar = document.querySelector(".sidebar");
+let heroUl = document.querySelector(".hero-ul");
+let login = document.querySelector(".login");
+let modal = document.querySelector(".modal");
+let elClose = document.querySelector(".close");
+let modalContent = document.querySelector(".modal_content");
 
 elMenyu.addEventListener("click", function () {
   sideBar.classList.toggle("svag");
-  console.log('bosildi');
-  
+  heroUl.classList.toggle("goss");
 });
+
+login.addEventListener("click", function () {});
+
+login.onclick = function () {
+  modal.style.display = "block";
+};
+
+elClose.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (e) {
+  if (e.target === modal || e.target === elClose) {
+    modal.style.display = "none";
+  }
+};
