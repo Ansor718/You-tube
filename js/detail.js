8454f3159e5fb46d949b5e7a5d58a4c56f8ca9d8
Ansor <ansorgulomov4@gmail.com>
2025-05-23 17:51:44 +0500

    sdfgh

--- js/detail.js
+++ js/detail.js
@@ -12,6 +12,30 @@ videoHTML.innerHTML = `
 
     `;
 
+let rightVideo = document.querySelector(".hero-ul");
+
+rightVideo.innerHTML = `
+  <li class="hero-li">
+                  <img src="./assets/Frame (5).svg" alt="" />
+
+                  <div class="hero-div">
+                    <div>
+                      <img src="./assets/Image.svg" alt="" />
+                    </div>
+
+                    <div>
+                      <h4 class="hero-new-attack-h4">
+                        New attack targets U.S. base in Syria <br />
+                        following American airstrikes over
+                      </h4>
+                      <p class="cbs-news-p">
+                        CBS New <br />126K views • 2 hours ago
+                      </p>
+                    </div>
+                  </div>
+                </li>
+`;
+
 let like = document.querySelector(".like");
 
 like.addEventListener("click", function () {
