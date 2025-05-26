4ba8f6a3e6faac0f2baa914ffd66133abe2e37dc
Ansor <ansorgulomov4@gmail.com>
2025-05-26 14:18:58 +0500

    asdfv

--- js/detail.js
+++ js/detail.js
@@ -9,6 +9,7 @@ console.log(element.src);
 
 videoHTML.innerHTML = `
         <iframe style="" width="914" height="514" src="${element.src}" title="Tirik kitob" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        
 
     `;
 
@@ -17,7 +18,22 @@ let rightVideo = document.querySelector(".hero-ul");
 rightVideo.insertAdjacentHTML(
   "beforeend",
   `
-    
+    <li class="hero-li">
+        <a href="./detail.html?id=${element.id}"><img class="hero__img" style="border-radius: 20px" width="400px" src="${element.thumbnail}" alt="" /></a>
+
+        <div class="hero-div">  
+          <div class="vd">
+            <img class="imghero" class="hero__profile-img" src="${element.channelPhoto}" alt="" />
+          </div>
+
+          <div>
+            <h4 class="hero-new-attack-h4" style="width: 281px;">
+              ${element.title}<br />
+            </h4>
+            <p class="cbs-news-p">${element.channel} <br />${element.views}  ${element.uploaded}</p>
+          </div>
+        </div>
+      </li>
   `
 );
 
